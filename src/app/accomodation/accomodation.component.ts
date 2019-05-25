69b2dbf04b26d185c69938759e3f244a96731b1c
Aman Kumar <bishnoia03@gmail.com>
2019-05-25 20:24:54 +0530

    25-05-2019

--- src/app/accomodation/accomodation.component.ts
+++ src/app/accomodation/accomodation.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {MatSnackBar, mixinColor,} from '@angular/material';
 
 @Component({
   selector: 'app-accomodation',
@@ -6,8 +8,38 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./accomodation.component.css']
 })
 export class AccomodationComponent implements OnInit {
+  message: string;
+  action: string;
+  countary: string;
+  viewcountary: string;
+  isLinear = true;
+  firstFormGroup: FormGroup;
+  secondFormGroup: FormGroup;
+  numb: number;
+  iota = 0;
+  constructor(private formBuilder: FormBuilder, private snackBar: MatSnackBar ) { }
+  mysnackBar() {
+    this.snackBar.open(this.message = 'Reservation Success!', this.action = '', {
+      duration: 2500 ,
 
-  constructor() { }
+    });
+  }
+  fun1(){
+    while(this.iota>0){ 
+this.iota = this.iota-1;
+    }
+  }
+  fun2(){
+    if(this.iota<3){ 
+    this.iota = this.iota+1;
+      }
+    }
   ngOnInit() {
+    this.firstFormGroup = this.formBuilder.group({
+      firstCtrl: ['', Validators.required]
+    });
+    this.secondFormGroup = this.formBuilder.group({
+      secondCtrl: ['', ]
+    });
   }
 }
--- src/app/food-and-beverage/food-and-beverage.component.ts
+++ src/app/food-and-beverage/food-and-beverage.component.ts
@@ -1,14 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+import {MatSnackBar, mixinColor,} from '@angular/material';
 @Component({
   selector: 'app-food-and-beverage',
   templateUrl: './food-and-beverage.component.html',
   styleUrls: ['./food-and-beverage.component.css']
 })
 export class FoodAndBeverageComponent implements OnInit {
+  message: string;
+  action: string;
+  
+  constructor(private snackBar: MatSnackBar ) { }
+  mysnackBar() {
+    this.snackBar.open(this.message = 'Thanks for Thumbs up', this.action = '', {
+      duration: 2500 ,
 
-  constructor() { }
-
+    });
+  }
   ngOnInit() {
   }
 
